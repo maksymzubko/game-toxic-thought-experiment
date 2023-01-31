fc732ee48d0792435d24debb5c907fdadef85e8c
paranoidkid (Max Zubko) <56440333+s1maxx@users.noreply.github.com>
2023-01-31 14:57:52 +0200

    added two instances of socket

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,11 @@ import {io} from "socket.io-client";
 import {setSocket, setUserId} from "./redux/store/socket/slice";
 import {useDispatch} from "react-redux";
 
-const socket = io('https://project15.aestar.com.ua:5015/')
+// local
+const socket = io('ws://localhost:3000', {transports: ['websocket']});
+
+// server
+// const socket = io('https://project15.aestar.com.ua:5016/', {transports: ['websocket']});
 
 function App() {
     const routes = useRoutes(r);
