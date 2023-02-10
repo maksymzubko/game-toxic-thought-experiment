9d4e372f31b67701e0268292f0e7dc8ea8bbf840
KeS334 <keba.serhiy.p@gmail.com>
2023-02-10 23:18:19 +0200

    confirm for delete question

--- src/containers/Profile/index.tsx
+++ src/containers/Profile/index.tsx
@@ -12,6 +12,8 @@ import addQuestionImg from './assets/addQuestion.png';
 import logoutImg from './assets/logout.png';
 import backArrow from "../../assets/back-arrow.png";
 import delIcon from "./assets/delIcon.png";
+import crossIcon from "./assets/cross-icon.png";
+import sadDogImg from './assets/dog-sad.png'
 
 import { questionsExample } from './exampleData'
 
@@ -23,6 +25,7 @@ const Profile = () => {
 
     const [currentStage, setCurrentStage] = useState<'list' | 'details' | 'edit'>('list');
     const [selectedQuestion, setSelectedQuestion] = useState({id: null, title: '', answers: ['', '', '','']});
+    const [confirmDelete, setConfirmDelete] = useState(false);
 
 
     const logout = () => {
@@ -60,69 +63,85 @@ const Profile = () => {
         goBack();
     }
 
+    const deleteQuestion = () => {
+        //send id of question to server for deleting
+        setConfirmDelete(false);
+        goBack();
+    }
 
     return (
-        <div className="profile">
-            <p className="title">
-                <img src={backArrow} alt="" onClick={() => goBack()} />
-                profile
-            </p>
-            {currentStage === 'list' &&
-                <>
-                    <div className="user-block">
-                        <img src={addQuestionImg} alt="" onClick={() => editQuestion()}/>
-                        <p className="user-name">User Name</p>
-                        <img src={logoutImg} alt="" onClick={() => logout()} />
-                    </div>
-                    <div className="questions-list">
-                        {questionsExample.map(item =>
-                            <div className="list-item" onClick={() => showDetails(item)}>
-                                {item.title}
-                            </div>
-                        )}
-                    </div>
-                </>
-            }
-            {currentStage === 'details' &&
-                <div className="details-block">
-                    <p className="details-title">{selectedQuestion && selectedQuestion.title}</p>
-                    <ul>
-                        {selectedQuestion && selectedQuestion.answers.map(answer => <li>{answer}</li>)}
-                    </ul>
-                    <div className="details-buttons-block">
-                        <img src={delIcon} alt="" onClick={() => playButton()} />
-                        <div className="edit-button" onClick={() => editQuestion()}>edit</div>
-                    </div>
+        <>
+            <div className="profile">
+                <p className="title">
+                    <img src={backArrow} alt="" onClick={() => goBack()} />
+                    profile
+                </p>
+                {currentStage === 'list' &&
+                    <>
+                        <div className="user-block">
+                            <img src={addQuestionImg} alt="" onClick={() => editQuestion()}/>
+                            <p className="user-name">User Name</p>
+                            <img src={logoutImg} alt="" onClick={() => logout()} />
+                        </div>
+                        <div className="questions-list">
+                            {questionsExample.map(item =>
+                                <div className="list-item" onClick={() => showDetails(item)}>
+                                    {item.title}
+                                </div>
+                            )}
+                        </div>
+                    </>
+                }
+                {currentStage === 'details' &&
+                    <div className="details-block">
+                        <p className="details-title">{selectedQuestion && selectedQuestion.title}</p>
+                        <ul>
+                            {selectedQuestion && selectedQuestion.answers.map(answer => <li>{answer}</li>)}
+                        </ul>
+                        <div className="details-buttons-block">
+                            <img src={delIcon} alt="" onClick={() => {playButton(); setConfirmDelete(true)}} />
+                            <div className="edit-button" onClick={() => editQuestion()}>edit</div>
+                        </div>
 
-                </div>
-            }
-            {currentStage === 'edit' &&
-                <div className="edit-block">
-                    <div className="edit-title-wrapper">
-                        <textarea
-                            value={selectedQuestion ? selectedQuestion.title : ''} placeholder="Your question"
-                            onChange={e => setSelectedQuestion({...selectedQuestion, title: e.target.value})}
-                        />
                     </div>
-                    {selectedQuestion.answers.map((answer, index) =>
-                        <div className="edit-answer-wrapper">
-                            <input
-                                value={selectedQuestion ? selectedQuestion.answers[index]: ''}
-                                placeholder={`option ${index + 1}`}
-                                onChange={e => {
-                                    const answerTemp = JSON.parse(JSON.stringify(selectedQuestion));
-                                    answerTemp.answers[index] = e.target.value;
-                                    setSelectedQuestion(answerTemp)
-                                }}
+                }
+                {currentStage === 'edit' &&
+                    <div className="edit-block">
+                        <div className="edit-title-wrapper">
+                            <textarea
+                                value={selectedQuestion ? selectedQuestion.title : ''} placeholder="Your question"
+                                onChange={e => setSelectedQuestion({...selectedQuestion, title: e.target.value})}
                             />
                         </div>
-                    )}
+                        {selectedQuestion.answers.map((answer, index) =>
+                            <div className="edit-answer-wrapper">
+                                <input
+                                    value={selectedQuestion ? selectedQuestion.answers[index]: ''}
+                                    placeholder={`option ${index + 1}`}
+                                    onChange={e => {
+                                        const answerTemp = JSON.parse(JSON.stringify(selectedQuestion));
+                                        answerTemp.answers[index] = e.target.value;
+                                        setSelectedQuestion(answerTemp)
+                                    }}
+                                />
+                            </div>
+                        )}
 
-                    <div className="done-button" onClick={() => saveQuestion()}>done</div>
+                        <div className="done-button" onClick={() => saveQuestion()}>done</div>
+                    </div>
+                }
+            </div>
+            {confirmDelete &&
+            <div className="confirm-delete">
+                <div className="modal">
+                    <img src={crossIcon} alt="" onClick={() => {playButton(); setConfirmDelete(false)}} className="close" />
+                    <img src={sadDogImg} alt="" className="sad-dog"/>
+                    <p>are you sure you want to delete this question</p>
+                    <div className="delete-button" onClick={() => deleteQuestion()}>delete</div>
                 </div>
+            </div>
             }
-
-        </div>
+        </>
     );
 };
 
